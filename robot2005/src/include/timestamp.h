6be3188ffb99c72e9239ed0ee46ea4c69f3c7b49
floitsch <floitsch>
2005-03-01 22:15:05 +0000

    those files are created automatically during build.

--- robot2005/src/include/timestamp.h
+++
@@ -1,8 +0,0 @@
-#ifndef TIMESTAMP_H
-#define TIMESTAMP_H
-#define __COMPILE_DATE__ "01/03/05"
-#define __COMPILE_TIME__ "21:06:24"
-#define __COMPILE_USER__ "lsm"
-#define __COMPILE_DIR__ "/home/lsm/berlios/robot2005/src"
-#define __COMPILE_HOST__ ""
-#endif
