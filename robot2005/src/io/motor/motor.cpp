09ecb526f3ac9cf950cefcacbbba086971fc1f5f
m_les_ronces <m_les_ronces>
2005-03-15 20:38:11 +0000

    add motor package

---
+++ robot2005/src/io/motor/motor.cpp
@@ -0,0 +1,145 @@
+#include "motor.h"
+#ifndef TELECOMMAND_MAIN
+#include "log.h"
+#endif
+#include "HCTLControl.h"
+#include "events.h"
+#include <unistd.h>
+
+
+static const int MOTOR_PWM_TIME_ALERT=80;
+
+// 80 = toutes les 2s
+
+// ============================================================================
+// =================================  class Motor   ===========================
+// ============================================================================
+
+/** Retourne true si le module est valide*/
+bool Motor::validate()
+{
+  int lapCount=0;
+  while(lapCount++<10){
+    setSpeed(10,10); 
+    MotorPosition pos1L, pos1R;
+    getPosition(pos1L, pos1R);
+    sleep(3);
+    setSpeed(0,0);
+    sleep(1);
+    setSpeed(-10,-10);
+    sleep(3);
+    setSpeed(0,0);
+    sleep(1);
+  }
+  return true;
+}
+
+/** Reset les hctl (gauche et droite) */
+bool Motor::reset()
+{
+  counterLeft_=0;
+  counterRight_=0;
+  init_=true; 
+  if (resetCallBack_) resetCallBack_();
+  return init_;
+}
+
+void Motor::resetPwmAlert()
+{
+    counterLeft_=0;
+    counterRight_=0;
+}
+
+/** Reset autimatiquement les hctl quand ils saturent trop longtemps */
+void Motor::enableAutomaticReset(bool enable)
+{
+  enableAutomaticReset_ = enable;
+}
+
+/** Enregistre un callback appele quand les hctl saturent trop longtemps */
+void Motor::registerPWMAlertFunction(MotorAlertFunction fn)
+{
+  alertFunction_ = fn;
+}
+
+/** Tache periodique qui verifie que les pwm ne depassent pas la limite */
+void Motor::checkMotorEvents()
+{
+  bool alert=false;
+  MotorPWM left=0;
+  MotorPWM right=0;
+  static MotorPWM lastLeft=0;
+  static MotorPWM lastRight=0;
+#ifndef TELECOMMAND_MAIN
+  // quand il y a l'arret d'urgence, on reset les hctl une seule fois
+  static int emergencyResetCounter=0;
+  static int emergencyResetCounter2=0;
+  if (EVENTS->check(EVENTS_EMERGENCY_STOP)) {
+      if (enableAutomaticReset_
+          && emergencyResetCounter2 == emergencyResetCounter) {
+          LOG_WARNING("Motor emergency stop reset\n");
+          emergencyResetCounter++;
+          reset();
+      }
+      return;
+  }
+  emergencyResetCounter2 = emergencyResetCounter;
+#endif
+
+  getPWM(left, right);
+  // test la roue gauche
+  if ((lastLeft>110 && left>110) ||
+      (lastLeft<-110 && left<-110)) {
+    if (counterLeft_++ > MOTOR_PWM_TIME_ALERT) {
+#ifndef TELECOMMAND_MAIN
+      LOG_ERROR("La roue gauche est bloquée\n");
+#endif
+      alert=true;
+    }
+  } else {
+    counterLeft_=0;
+  }
+  // test la roue droite
+  if ((lastRight>110 && right>110) ||
+      (lastRight<-110 && right<-110)) {
+    if (counterRight_++ > MOTOR_PWM_TIME_ALERT) {
+#ifndef TELECOMMAND_MAIN
+      LOG_ERROR("La roue droite est bloquée\n");
+#endif
+      alert=true;
+    }
+  } else {
+    counterRight_=0;
+  }
+  lastLeft=left;
+  lastRight=right;
+  // on a un probleme sur une roue
+  if (alert) {
+    counterLeft_=0;
+    counterRight_=0;
+    if (enableAutomaticReset_) {
+      reset();
+    }
+#ifndef TELECOMMAND_MAIN
+    EVENTS->raise(EVENTS_PWM_ALERT);
+#endif
+    if (alertFunction_) {
+      alertFunction_(left, right);
+    }
+  }
+}
+
+/** Constructeur */
+Motor::Motor(bool   automaticReset, 
+	     MotorAlertFunction fn) : 
+  RobotIODevice("Motor", CLASS_MOTOR), 
+  alertFunction_(fn), enableAutomaticReset_(automaticReset), 
+  counterLeft_(0), counterRight_(0), resetCallBack_(NULL)
+{
+}
+
+void Motor::registerResetCallback(FunctionPtr cb)
+{
+    resetCallBack_ = cb;
+}
+
---
+++ robot2005/src/io/motor/motorSimu.cpp
@@ -0,0 +1,60 @@
+#include "motor.h"
+#include "log.h"
+#include "simulatorClient.h"
+
+// ============================================================================
+// ==============================  class MotorSimu   ==========================
+// ============================================================================
+
+/** Reset les hctl (gauche et droite) */
+bool MotorSimu::reset()
+{
+  LOG_FUNCTION();
+  Motor::reset();
+  
+  init_=true;
+  return init_;
+}
+
+/** Defini la constante d'acceleration des moteurs */
+void MotorSimu::setAcceleration(MotorAcceleration acceleration)
+{
+
+}
+
+/** Specifie un consigne en vitesse pour les moteurs */
+void MotorSimu::setSpeed(MotorSpeed left, 
+			 MotorSpeed right)
+{
+  LOG_DEBUG("set speed(%d, %d)\n", (int)left, (int)right);
+  //Simulator::instance()->setSpeed(left, right);
+}
+
+/** Retourne la position des codeurs */
+void MotorSimu::getPosition(MotorPosition &left,
+                            MotorPosition &right)
+{
+  //Simulator::instance()->getMotorPosition(left, right);
+}
+
+/** Retourne la consigne reellement envoyee au moteurs */
+void MotorSimu::getPWM(MotorPWM &left,
+		       MotorPWM &right)
+{
+  //Simulator::instance()->getPwm(left, right);
+}
+
+/** desasserrvit les moteurs */
+void MotorSimu::idle()
+{
+}
+
+MotorSimu::MotorSimu(bool   automaticReset, 
+		     MotorAlertFunction fn) :
+  Motor(automaticReset, fn)
+{
+  LOG_FUNCTION();
+  reset();
+  LOG_OK("Initialisation terminée\n");
+}
+
