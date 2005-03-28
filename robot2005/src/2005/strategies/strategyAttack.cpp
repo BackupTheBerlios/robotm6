cb0013862c61bb1fcf393c28cac201d6fe78c4ba
m_les_ronces <m_les_ronces>
2005-03-28 18:18:12 +0000

    tart implementation of gotoBridge

--- robot2005/src/2005/strategies/strategyAttack.cpp
+++ robot2005/src/2005/strategies/strategyAttack.cpp
@@ -9,7 +9,8 @@
 
 StrategyAttackCL::StrategyAttackCL(RobotMainCL* main): 
     Strategy2005CL("StrategyAttack", "Robot attack", CLASS_STRATEGY, main),
-    bridgeAvailibility_(0xFF), bridge_(BRIDGE_POS_UNKNOWN), useLeftBridge_(true)
+    bridgeAvailibility_(0xFF), bridge_(BRIDGE_POS_UNKNOWN), 
+    useLeftBridge_(true), bridgeDetectionByCenter_(true)
 {
 
 }
@@ -61,4 +62,139 @@ void StrategyAttackCL::run(int argc, char* argv[])
     Log->emergencyStopPressed(true);
     sleep(5);
     return;
+}
+
+// ================ Tire des catapultes ==================
+/** @brief Tire des balles avec les catapultes */
+void StrategyAttackCL::fireCatapults()
+{
+}
+
+static const Millimeter xToDetectBridgeBySharp = 1300;
+// ================ Detection et traversee du pont ==================
+
+/** @brief va a l'endroit ou on detecte les pont par capteurs sharps */
+bool StrategyAttackCL::gotoBridgeDetection()
+{
+  Trajectory t;
+  t.push_back(Point(RobotPos->x(), RobotPos->y()));
+  t.push_back(Point(900, RobotPos->y()));
+  if (bridgeDetectionByCenter_) {
+    // va vers le pont du milieu
+    t.push_back(Point(xToDetectBridgeBySharp-250,1275));
+    t.push_back(Point(xToDetectBridgeBySharp,1275));
+  } else {
+    // va vers la gauche du terrain pour detecter la position du pont d'un coup
+    t.push_back(Point(xToDetectBridgeBySharp-250,1725));
+    t.push_back(Point(xToDetectBridgeBySharp,1725));
+  }
+  Move->followTrajectory(t/*, TRAJECTORY_SPLINE*/);
+  Events->wait(evtEndMove);
+  // todo manage events
+  Move->stop();
+  return false;
+}
+/** @brief met a jour la variable bridge_ en fonction de ce que disent les 
+    sharps */
+bool StrategyAttackCL::getBridgePosBySharp()
+{
+/*
+  BRIDGE_POS_BORDURE=1,        
+  BRIDGE_POS_MIDDLE_BORDURE=2,
+  BRIDGE_POS_MIDDLE_CENTER=3,
+  BRIDGE_POS_CENTER=4
+*/
+  if (bridgeDetectionByCenter_) {
+    
+  } else {
+
+  }
+  // TODO
+  bridge_=BRIDGE_POS_CENTER; // test
+  return false;
+}
+/** @brief se place en face du pont ou on peut utiliser les bumpers pour 
+    savoir si le pont est bien la. La variable bridge doit deja etre mise 
+    a jour */
+bool StrategyAttackCL::gotoBridgeEntry(Millimeter y, 
+				       bool rotateLeft, 
+				       bool rotateRight)
+{
+  if (fabs(RobotPos->y() - y)<50) {
+    Move->go2Target(Point(1470, y));
+    Events->wait(evtEndMove);
+  } else {
+    // verifier la direction:
+  //  Move->
+    Events->wait(evtEndMove);
+    // TODO
+    Move->go2Target(Point(1470, y));
+    Events->wait(evtEndMove);
+  }
+  
+  return false;
+}
+/** @brief se place en face du pont ou on peut utiliser les bumpers pour 
+    savoir si le pont est bien la. La variable bridge doit deja etre mise 
+    a jour */
+bool StrategyAttackCL::gotoBridgeEntry()
+{
+  if (bridgeDetectionByCenter_) {
+    if (bridge_ == BRIDGE_POS_CENTER || 
+	bridge_ == BRIDGE_POS_MIDDLE_CENTER) {
+      gotoBridgeEntry(1275);
+    } else if (bridge_ == BRIDGE_POS_MIDDLE_BORDURE) {
+      gotoBridgeEntry(1725);
+    } else if (bridge_ == BRIDGE_POS_BORDURE) {
+      gotoBridgeEntry(1875, true);
+    }
+  } else {
+    if (bridge_ == BRIDGE_POS_CENTER) {
+      gotoBridgeEntry(1425);
+    } else if (bridge_ == BRIDGE_POS_MIDDLE_CENTER) {
+      gotoBridgeEntry(1575);
+    } else if (bridge_ == BRIDGE_POS_MIDDLE_BORDURE) {
+      gotoBridgeEntry(1725);
+    } else if (bridge_ == BRIDGE_POS_BORDURE) {
+      gotoBridgeEntry(1875, true);
+    }
+  }
+  return false;
+}
+/** @brief verifie que le pont est bien la ! */
+bool StrategyAttackCL::getBridgePosByBumper()
+{
+  return false;
+}
+/** @brief traverse le pont : detection de collisions...*/
+bool StrategyAttackCL::crossBridge()
+{
+  return false;
+}
+/** @brief va jusq'a l'aute pont et change l'etat de la variable
+    useLeftBridge_ */
+bool StrategyAttackCL::gotoOtherBridge()
+{
+  return false;
+}
+
+// ================ Exploration de la zone ennemie ==================
+
+/** @brief explore juste les zone ou il peut y avoir des quilles 
+    d'apres le reglement */
+bool StrategyAttackCL::basicSkittleExploration()
+{
+  return false;
+}
+/** @brief se promene sur le terrain adverse en allant voir partout: sur
+    les bords et chez l'adversaire */
+bool StrategyAttackCL::extendedSkittleExploration()
+{
+  return false;
+}
+/** @brief va faire tomber les quilles du milieu du terrain 
+    (la pile de 3) */
+bool StrategyAttackCL::killCenterSkittles()
+{
+  return false;
 }
