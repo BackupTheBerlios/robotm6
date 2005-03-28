2962304ad47fb74115aea8b6dfe21fb25d5e8eed
floitsch <floitsch>
2005-03-28 18:11:03 +0000

    added periodic-task thread. some changes in initialisation-code.

--- robot2005/src/2005/base/robotDevices.cpp
+++ robot2005/src/2005/base/robotDevices.cpp
@@ -5,13 +5,46 @@
 #include "implementation/soundSimu.h"
 #include "implementation/lcdSimu.h"
 #include "implementation/odometerSimu.h"
+#include "devices/implementation/odometer04.h"
+#include "devices/implementation/lcd03.h"
+#include "io/ioManager.h"
+#include "io/serialPort.h"
+#include "log.h"
+
+namespace {
+    // TODO: copied the name from ioManager. what's the meaning of the "4"? [flo]
+    pthread_mutex_t repositoryLock4 = PTHREAD_MUTEX_INITIALIZER;
+    void no_op(...){};
+}
+
+void* RobotDevicesThreadBody(void* robotDevices)
+{
+    while(1)
+    {
+	// TODO: remove magic number and replace with constant [flo]
+	usleep(10000);
+	static_cast<RobotDevicesCL*>(robotDevices)->periodicTask();
+    }
+    return NULL;
+}
 
 RobotDevicesCL::RobotDevicesCL() :
     RobotBase("Robot devices", CLASS_ROBOT_DEVICES),
     motor_(NULL), odometer_(NULL), lcd_(NULL), bumper_(NULL), sound_(NULL)
-                                   
 {
-    
+    LOG_FUNCTION();
+    thread_ = 0;
+    bool init = (MTHREAD_CREATE("RobotDevices Thread",
+				&thread_,
+				NULL,
+				RobotDevicesThreadBody,
+				this)
+		 == 0);
+    if (init) {
+	LOG_OK("Initialisation Done\n");
+    } else {
+	LOG_ERROR("Can't run RobotDevices-thread.");
+    }
 }
 
 RobotDevicesCL::~RobotDevicesCL()
@@ -26,8 +59,12 @@ RobotDevicesCL::~RobotDevicesCL()
 /** @brief detecte et cree toutes les cartes */
 void RobotDevicesCL::allocDevices()
 {
-    // TODO
+    LOG_FUNCTION();
 
+//    IoManager->submitIoHost(new SerialPort(0, false));
+    // TODO: keep reference for later deletion of serialPort
+    IoManager->submitIoHost(new SerialPort(1, false));
+    
     if (RobotConfig2005->motorSimu) {
         motor_ = new MotorSimu(RobotConfig->automaticMotorReset);
     } else {
@@ -39,12 +76,22 @@ void RobotDevicesCL::allocDevices()
     }
     if (RobotConfig2005->lcdSimu) {
         lcd_   = new LcdSimu();
+    } else {
+	//lcd_ = new LcdCL();
+	lcd_ = new Lcd_03();
     }
+
     if (RobotConfig2005->soundSimu) {
         sound_   = new SoundSimu();
+    } else {
+	sound_ = new SoundCL();
     }
+
     if (RobotConfig2005->odometerSimu) {
-        odometer_ = new OdometerSimu();
+	odometer_ = new OdometerSimu();
+    } else {
+	odometer_ = new Odometer_04();
+    	//odometer_ = new OdometerCL();
     }
 }
 
@@ -66,4 +113,14 @@ void RobotDevicesCL::reset()
     if (lcd_)      lcd_->reset();
     if (bumper_)   bumper_->reset();
     if (sound_)    sound_->reset();
+}
+
+void RobotDevicesCL::periodicTask()
+{
+    Lock localLock(&repositoryLock4);
+    RobotDevicesSet::iterator it;
+    for (it = watchedDevices_.begin();
+	 it != watchedDevices_.end();
+	 ++it)
+	(*it)->periodicTask();
 }
