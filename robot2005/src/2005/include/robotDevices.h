e83038685926bfaffe3e17249439e022d9286c5d
floitsch <floitsch>
2005-03-28 18:12:59 +0000

    redirected includes. added stuff for periodic-task stuff.

--- robot2005/src/2005/include/robotDevices.h
+++ robot2005/src/2005/include/robotDevices.h
@@ -1,12 +1,14 @@
 #ifndef __ROBOT_DEVICES_H__
 #define __ROBOT_DEVICES_H__
 
+#include "mthread.h"
+#include <set>
 #include "robotBase.h"
-#include "lcd.h"
-#include "odometer.h"
-#include "motor.h"
-#include "bumper.h"
-#include "sound.h"
+#include "devices/lcd.h"
+#include "devices/odometer.h"
+#include "devices/motor.h"
+#include "devices/bumper.h"
+#include "devices/sound.h"
 
 /** @brief Initialisation des cartes electroniques */
 class RobotDevicesCL : public RobotBase {
@@ -29,7 +31,19 @@ class RobotDevicesCL : public RobotBase {
     BumperCL*   getBumper() const   { return bumper_; }
     SoundCL*    getSound() const    { return sound_; }
 
- private:
+protected: // periodic
+    friend void* RobotDevicesThreadBody(void*);
+    void periodicTask();
+
+    void startWatching(RobotDeviceCL* device);
+    void stopWatching(RobotDeviceCL* device);
+
+protected: // periodic fields
+    pthread_t thread_;
+    typedef std::set<RobotDeviceCL*> RobotDevicesSet;
+    RobotDevicesSet watchedDevices_;
+    
+private:
     MotorCL*    motor_;
     OdometerCL* odometer_;
     LcdCL*      lcd_;
