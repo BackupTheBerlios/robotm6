99639e79d1f37deb8783fa7a5bd3c49fce4bc86c
floitsch <floitsch>
2005-04-16 14:19:40 +0000

    ang communication protocole with bumper & sound

--- robot2005/src/include/driver/bumperCom_05.h
+++ robot2005/src/include/driver/bumperCom_05.h
@@ -12,10 +12,18 @@ static const unsigned char BUMPER_REQ_GET_DATA1  = 0x01;
 static const unsigned char BUMPER_REQ_GET_DATA2  = 0x02;
 // demande de renvoyer le 3eme octet
 static const unsigned char BUMPER_REQ_GET_DATA3  = 0x04;
-// demande de renvoyer les 3 octects de donnees
+// demande de renvoyer les 3 octects de donnees + 4 bits de start 
+// + 4 bits de checksum
+// 0xA0+(data[0]&0x0F) : start + 4bits de poids faible de data[0]
+// data[1]
+// data[2]
+// (data[0]&0xF0) + 4 bits de checksum : 
+//          checksum = data[0]^data[1]^data[2]  (^=xor)
+//          4 bits de checksum= (checksum&0x0F)^((checksum&0xF0)>>4)
 static const unsigned char BUMPER_REQ_GET_ALL    = 0x07;
 
 // nombre d'octet maximal
 static const unsigned int  BUMPER_DATA_NBR  = 3;
+static const unsigned char  BUMPER_START_BITS  = 0xA0;
 
 #endif // __BUMPER_COM_05_H__
--- robot2005/src/include/driver/soundCom_05.h
+++ robot2005/src/include/driver/soundCom_05.h
@@ -2,12 +2,27 @@
 #define __SOUND_COM_05_H__
 
 // carte son 2005
+/*
+-ping: le pc envoie 0xAA, la carte repond 0xC3  
+
+-playSound: le pc envoie 0xXX  %11XXXXXX   XXXXX numero du titre, la carte ne repond rien
+  => la carte met le son dans sa pile pour le jouer quand les sons precedents seront termines (prevoir une pile d'au moins 5 sons)
+
+-stop: le pc envoie 0x00, la carte ne repond rien
+  => le son/musique actuelle est arrete et la pile de son est effacee
+
+-playRandomMusic: le pc envoie 0x80, la carte ne repond rien
+  => la carte joue des musiques au hasard jusqu'à ce que la commande stop ou playSound soit appelee
+
+Quand la carte ne joue pas de son ou de musique depuis plus de 5 secondes, elle joue un son au hasard (attention ne pas jouer les sons d'alertes...)
+*/
 
 static const unsigned char SOUND_REQ_PING = 0xAA;
 static const unsigned char SOUND_PING_ID  = 0xC3;
 
-// demande la tension des 4 batteries : renvoie 4 octects (1 octect par batterie)
-static const unsigned char SOUND_REQ_PLAY  = 0x00;
-static const unsigned char SOUND_REQ_CLEAR_STACK  = 0x0FF;
+//
+static const unsigned char SOUND_REQ_PLAY  = 0xC0; // 11xx xxxx
+static const unsigned char SOUND_REQ_CLEAR_STACK  = 0x00;
+static const unsigned char SOUND_REQ_MUSIC  = 0x80;
 
 #endif // __SOUND_COM_05_H__
