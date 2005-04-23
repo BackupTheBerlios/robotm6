968627af282ba21c915dceb856bdc830e846fd78
floitsch <floitsch>
2005-04-23 23:30:26 +0000

    improved tests.

--- robot2005/src/main/testIo/testIo.cpp
+++ robot2005/src/main/testIo/testIo.cpp
@@ -1,12 +1,33 @@
 #define LOG_CLASSID CLASS_DEFAULT
-#include "log.h"
+//#include "log.h"
 #include "io/serialPort.h"
 #include "io/ioManager.h"
 
+void printLoop(IoDevice* device) {
+    device->open();
+    device->write(0xAA);
+    unsigned char c;
+    while (true) {
+	device->read(&c);
+	printf("0x%2.2x ", c);
+    }
+    device->close();
+}
+
 int main(int argc, char* argv[])
 {
-    IoManager->submitIoHost(new SerialPort(0));
-    IoManager->submitIoHost(new SerialPort(2));
-    IoManager->submitIoHost(new SerialPort(3));
-    return 0;
+    if (argc == 1) {
+	IoManager->submitIoHost(new SerialPort(0));
+	IoManager->submitIoHost(new SerialPort(2));
+	IoManager->submitIoHost(new SerialPort(3));
+	return 0;
+    } else if (argc == 2) {
+	IoManager->submitIoHost(new SerialPort(atoi(argv[1])));
+	return 0;
+    } else if (argc == 3) {
+	IoHost* host = new SerialPort(atoi(argv[1]), SERIAL_SPEED_38400);
+	IoDevice* device = host->listPorts()[0];
+	printLoop(device);
+	return 0;
+    }
 }
