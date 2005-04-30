d95a0e195eedad85138f6d8feaab1e7b1fade234
floitsch <floitsch>
2005-04-30 00:30:45 +0000

    improvements and bug-fixes.

--- robot2005/src/2005/strategies/attackLargeExplore.cpp
+++ robot2005/src/2005/strategies/attackLargeExplore.cpp
@@ -141,15 +141,17 @@ bool StrategyLargeAttackCL::calcSupportCenterCollision(Point pos, Point* support
 }
 
 bool StrategyLargeAttackCL::centerOnSupport(Point supportCenter) {
-    const int XY_SQUARE_SAFETY = 100;
+    LOG_FUNCTION();
+    const int XY_SQUARE_SAFETY = 100; // allow some distance
     const Radian THETA_DELTA = M_PI_4/3;
     unsigned int retries = 0;
     // TODO: get MOVE_XY_SQUARE_EPSILON from move.h
     bool centered;
-    while (centered = (Geometry2D::getSquareDistance(RobotPos->pt(),
-						     supportCenter)
-		       > (MOVE_XY_SQUARE_EPSILON + XY_SQUARE_SAFETY))
+    while (!(centered = (Geometry2D::getSquareDistance(RobotPos->pt(),
+						       supportCenter)
+			 < (MOVE_XY_SQUARE_EPSILON + XY_SQUARE_SAFETY)))
 	   && retries < 2) {
+	LOG_INFO("centerOnSupport try %d\n", retries);
 	retries++;
 	Point currentPos = RobotPos->pt();
 	// try to go there directly
@@ -167,6 +169,7 @@ bool StrategyLargeAttackCL::centerOnSupport(Point supportCenter) {
 	    {
 		// turn on wheel
 		bool collisionLeft = (collision == COLLISION_LEFT);
+		LOG_INFO("collision (%s) while centering on support\n", collisionLeft?"left":"right");
 		MotorDirection dir = MvtMgr->getMotorDirection();
 		bool forward = (dir == MOTOR_DIRECTION_FORWARD);
 		bool stopLeft = collisionLeft;
@@ -184,21 +187,24 @@ bool StrategyLargeAttackCL::centerOnSupport(Point supportCenter) {
 	    }
 	}
     }
+    LOG_INFO("leaving centerSupport (%s)\n", centered?"success":"failed");
     return centered;
 }
 
 bool StrategyLargeAttackCL::rotateOnSupport(Point supportCenter,
 					    Radian targetTheta) {
+    LOG_FUNCTION();
     Point currentPos = RobotPos->pt();
     const int MAX_ROTATE_DELTA_SQUARE = 500;
     unsigned int retries = 0;
     bool rotated;
-    while (rotated = (fabs(RobotPos->thetaAbsolute() - targetTheta)
-		      > MOVE_ROTATION_EPSILON)
+    while (!(rotated = (fabs(RobotPos->thetaAbsolute() - targetTheta)
+			< MOVE_ROTATION_EPSILON))
 	   && retries < 2) {
 	retries++;
 	if (Geometry2D::getSquareDistance(supportCenter, currentPos)
 	    > MAX_ROTATE_DELTA_SQUARE) {
+	    LOG_INFO("rotateOnSupport: centering first.\n");
 	    // too far away from center
 	    if (RobotPos->isTargetForward(supportCenter))
 		MvtMgr->setRobotDirection(MOVE_DIRECTION_FORWARD);
@@ -207,7 +213,7 @@ bool StrategyLargeAttackCL::rotateOnSupport(Point supportCenter,
 	    Move->go2Target(supportCenter);
 	    Events->wait(evtEndMove);
 	    if (!Events->isInWaitResult(EVENTS_MOVE_END)) {
-		if (checkEndEvents()) return false;
+		if (checkEndEvents()) return false; // end of game
 		// don't really know, what else to do here. just try to rotate.
 	    }
 	}
@@ -224,6 +230,7 @@ bool StrategyLargeAttackCL::rotateOnSupport(Point supportCenter,
 	    {
 		// rotate counter-direction
 		bool collisionLeft = (collision == COLLISION_LEFT);
+		LOG_INFO("collision (%s) in rotateOnSupport\n", collisionLeft?"left":"right");
 		MotorDirection dir = MvtMgr->getMotorDirection();
 		bool turningLeft = (dir == MOTOR_DIRECTION_LEFT);
 		Radian counterTheta = COUNTER_THETA_DELTA
@@ -234,6 +241,7 @@ bool StrategyLargeAttackCL::rotateOnSupport(Point supportCenter,
 		if (!Events->isInWaitResult(EVENTS_MOVE_END))
 		    if (checkEndEvents()) return false; // end of match.
 	    } else if (collision == COLLISION_BOTH) {
+		LOG_INFO("collision on both sides. counter-rotate\n");
 		// rotate slightly counterDirection and continue loop
 		MotorDirection dir = MvtMgr->getMotorDirection();
 		Radian counterTheta = COUNTER_THETA_DELTA
@@ -243,16 +251,19 @@ bool StrategyLargeAttackCL::rotateOnSupport(Point supportCenter,
 		if (!Events->isInWaitResult(EVENTS_MOVE_END))
 		    if (checkEndEvents()) return false; // end of match
 	    } else if (collision == COLLISION_NONE) {
+		LOG_INFO("end-move but no collision. -> return false \n");
 		// no idea what happened -> return false
 		return false;
 	    }
 	}
     }
+    LOG_INFO("leaving rotateOnSupport (%s)\n", rotated?"success":"failed");
     return rotated;
 }
 
 // should be easy...
 bool StrategyLargeAttackCL::leaveSupport(Point supportCenter, Point target) {
+    LOG_FUNCTION();
     const Radian THETA_DELTA = M_PI_4/3;
     MvtMgr->setRobotDirection(MOVE_DIRECTION_FORWARD);
     Move->go2Target(target);
@@ -267,6 +278,7 @@ bool StrategyLargeAttackCL::leaveSupport(Point supportCenter, Point target) {
 	{
 	    // turn on wheel
 	    bool collisionLeft = (collision == COLLISION_LEFT);
+	    LOG_INFO("Collision (%s) in leaveSupport\n", collisionLeft?"left":"right");
 	    bool stopLeft = collisionLeft;
 	    Radian deltaTheta = THETA_DELTA* (collisionLeft? 1: -1);
 	    Move->rotateOnWheel(RobotPos->thetaAbsolute() + deltaTheta, stopLeft);
@@ -286,14 +298,49 @@ bool StrategyLargeAttackCL::preDefinedSkittleExploration2()
 {
     LOG_COMMAND("preDefinedLargeSkittleExploration2\n");
     MvtMgr->setRobotDirection(MOVE_DIRECTION_FORWARD);
-    Move->go2Target(2300, 1500); // our target-line is y=1500.
+    bool noRotation = true;
+    Move->go2Target(2500, 1500, // our target-line is y=1500.
+		    MOVE_USE_DEFAULT_GAIN,
+		    MOVE_USE_DEFAULT_SPEED,
+		    noRotation);
     Events->wait(evtEndMove);
     if (!Events->isInWaitResult(EVENTS_MOVE_END)) {
         // on n'a pas reussi
         // c'est la fin du match?
         if (checkEndEvents()) return false;
-        // TODO manage collisions
+
+	// TODO: check collision (as if on right side -> center on support ...)
         return false;
+    }
+
+    // only if no collision had been detected:
+    Move->rotate(0); // face right border
+    Events->wait(evtEndMove);
+    if (!Events->isInWaitResult(EVENTS_MOVE_END)) {
+	if (checkEndEvents()) return false; // end of match
+	// ok. normally the collision can only be on the left side...
+	CollisionEnum collision = checkCollisionEvents();
+	if (collision == COLLISION_NONE) {
+	    LOG_INFO("unhandled event. leaving function\n");
+	} else if (collision == COLLISION_LEFT) {
+	    // we probably found a support
+	    // go directly on support.
+	    Point supportCenter(2594, 1650);
+	    bool centered = centerOnSupport(supportCenter);
+	    if (!centered) return false;
+	    bool rotated = rotateOnSupport(supportCenter, -M_PI_4);
+	    if (!rotated) return false;
+
+	    // go to next square-intersection.
+	    Move->go2Target(supportCenter.x + 150, supportCenter.y + 150);
+	    Events->wait(evtEndMove);
+	    if (!Events->isInWaitResult(EVENTS_MOVE_END))
+		// for now just return false.
+		return false;
+	} else {
+	    // TODO...
+	    return false;
+	}
     }
 
     bool endOfLine = false;
@@ -311,6 +358,7 @@ bool StrategyLargeAttackCL::preDefinedSkittleExploration2()
 		// don't know, why we could get here...
 		return false;
 	    } else if (collision == COLLISION_LEFT) {
+		LOG_INFO("collision left\n");
 		Point leftWheel = getLeftWheelPos();
 		Point supportCenter;
 		bool doesIntersect = calcSupportCenterCollision(leftWheel, &supportCenter);
